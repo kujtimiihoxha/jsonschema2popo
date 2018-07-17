4f1e7ee3a812c478f71e2b0dab164f29c5fba7af
Kujtim Hoxha <kujtimhoxha@Kujtims-MacBook-Pro.local>
2018-07-17 20:38:04 +0200

    small changes, make all prop names snake case

--- jsonschema2popo/jsonschema2popo.py
+++ jsonschema2popo/jsonschema2popo.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-import os
 import argparse
 import json
+import os
+import re
+
 import networkx
 from jinja2 import Environment, FileSystemLoader
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 class JsonSchema2Popo:
     """Converts a JSON Schema to a Plain Old Python Object class"""
@@ -22,10 +25,10 @@ class JsonSchema2Popo:
         'boolean': bool,
         'null': None
     }
-    
+
     def __init__(self):
         self.jinja = Environment(loader=FileSystemLoader(searchpath=SCRIPT_DIR), trim_blocks=True)
-        
+
         self.definitions = []
 
     def load(self, json_schema_file):
@@ -44,7 +47,7 @@ class JsonSchema2Popo:
         for _obj_name, _obj in json_schema['definitions'].items():
             model = self.definition_parser(_obj_name, _obj)
             self.definitions.append(model)
-        
+
         # topological oderd dependencies
         g = networkx.DiGraph()
         models_map = {}
@@ -55,12 +58,12 @@ class JsonSchema2Popo:
                 g.add_edge(model['name'], '')
             for dep in deps:
                 g.add_edge(model['name'], dep)
-        
+
         self.definitions = []
         for model_name in networkx.topological_sort(g, reverse=True):
             if model_name in models_map:
                 self.definitions.append(models_map[model_name])
-        
+
         # root object
         if 'title' in json_schema:
             root_object_name = ''.join(x for x in json_schema['title'].title() if x.isalpha())
@@ -70,8 +73,7 @@ class JsonSchema2Popo:
         self.definitions.append(root_model)
 
     def definition_parser(self, _obj_name, _obj):
-        model = {}
-        model['name'] = _obj_name
+        model = {'name': _obj_name}
         if 'type' in _obj:
             model['type'] = self.type_parser(_obj)
 
@@ -88,19 +90,19 @@ class JsonSchema2Popo:
                 _enum = None
                 if 'enum' in _prop:
                     _enum = _prop['enum']
-                
+
                 _format = None
                 if 'format' in _prop:
                     _format = _prop['format']
                 if _type['type'] == list and 'items' in _prop and isinstance(_prop['items'], list):
                     _format = _prop['items'][0]['format']
-                
+
                 prop = {
-                    '_name': _prop_name,
+                    '_name': to_snake_case(_prop_name),
                     '_type': _type,
                     '_default': _default,
                     '_enum': _enum,
-                    '_format' : _format
+                    '_format': _format
                 }
                 model['properties'].append(prop)
         return model
@@ -135,16 +137,22 @@ class JsonSchema2Popo:
                 _type = self.J2P_TYPES[t['type']]
         elif '$ref' in t:
             _type = t['$ref'].split('/')[-1]
-        elif 'anyOf' in t or  'allOf' in t or  'oneOf' in t:
+        elif 'anyOf' in t or 'allOf' in t or 'oneOf' in t:
             _type = list
-        return { 'type': _type, 'subtype': _subtype }
+        return {'type': _type, 'subtype': _subtype}
 
     def write_file(self, filename):
         self.jinja.get_template(self.CLASS_TEMPLATE_FNAME).stream(models=self.definitions).dump(filename)
 
-class readable_dir(argparse.Action):
+
+def to_snake_case(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+class ReadableDir(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        prospective_dir=values
+        prospective_dir = values
         if not os.path.isdir(prospective_dir):
             raise argparse.ArgumentTypeError("readable_dir:{} is not a valid path".format(prospective_dir))
         if os.access(prospective_dir, os.R_OK):
@@ -152,10 +160,13 @@ class readable_dir(argparse.Action):
         else:
             raise argparse.ArgumentTypeError("readable_dir:{} is not a readable dir".format(prospective_dir))
 
+
 def init_parser():
     parser = argparse.ArgumentParser(description="Converts JSON Schema to Plain Old Python Object")
-    parser.add_argument('json_schema_file', type=argparse.FileType('r', encoding='utf-8'), help="Path to JSON Schema file to load")
-    parser.add_argument('-o', '--output-file', type=argparse.FileType('w', encoding='utf-8'), help="Path to file output", default="model.py")
+    parser.add_argument('json_schema_file', type=argparse.FileType('r', encoding='utf-8'),
+                        help="Path to JSON Schema file to load")
+    parser.add_argument('-o', '--output-file', type=argparse.FileType('w', encoding='utf-8'),
+                        help="Path to file output", default="model.py")
     return parser
 
 
@@ -168,6 +179,7 @@ def main():
 
     outfile = args.output_file
     loader.write_file(outfile)
+
 
 if __name__ == '__main__':
     main()
